22c523dc459cee37226f26d1b62bd9f09b61db93
Ilbert130 <ilbertca27@gmail.com>
2023-06-13 19:01:01 -0400

    aut module configured

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,44 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     ConfigModule,
+
+    //Mongoose
     MongooseModule.forFeature([
       {
         name: User.name,
         schema: UserSchema
       }
-    ])
+    ]),
+
+    //JWT 
+    PassportModule.register({defaultStrategy:'jwt'}),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory:(configService:ConfigService) => {
+
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn:'2h'
+          }
+        }
+      }
+    })
   ],
-  exports:[]
+  exports:[MongooseModule, JwtStrategy, PassportModule, JwtModule]
 })
 export class AuthModule {}
