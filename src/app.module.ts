aecb92c0ff13c3a7494aabbb9e0ac12ead5d5b10
Ilbert130 <ilbertca27@gmail.com>
2023-06-13 16:04:49 -0400

    model user created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+
+    //Configuring .env
+    ConfigModule.forRoot(),
+
+    //static content
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    //Configuring the db
+    MongooseModule.forRoot(process.env.MONGODB),
+
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
