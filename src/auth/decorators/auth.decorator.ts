90c17ee8e14c71d99a072fc356c569e075d05565
Ilbert130 <ilbertca27@gmail.com>
2023-06-13 18:54:13 -0400

    guard user-role created

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,12 @@
+import { applyDecorators } from "@nestjs/common";
+import { ValidRoles } from "../interfaces/valid-roles";
+import { RoleProtected } from "./role-protected.decorator";
+
+
+
+export function Auth(...roles:ValidRoles[]) {
+    return applyDecorators(
+        RoleProtected(...roles),
+        
+    );
+}
